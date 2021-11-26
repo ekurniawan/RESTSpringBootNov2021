5e5a657db62980dd12afce0b832ca2f015a294ad
Erick Kurniawan <erick.kurniawan@gmail.com>
2021-11-26 10:29:38 +0700

    menambahkan jwtuserdetails services

---
+++ src/main/java/id/actualtraining/restwithjpa/service/impl/JwtUserDetailsService.java
@@ -0,0 +1,38 @@
+package id.actualtraining.restwithjpa.service.impl;
+
+import id.actualtraining.restwithjpa.entity.DAOUser;
+import id.actualtraining.restwithjpa.model.UserDTO;
+import id.actualtraining.restwithjpa.repository.UserDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.ArrayList;
+
+public class JwtUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserDao userDao;
+
+    @Autowired
+    private PasswordEncoder bcryptEncoder;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        DAOUser user = userDao.findByUsername(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found with username: " + username);
+        }
+        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(),
+                new ArrayList<>());
+    }
+
+    public DAOUser save(UserDTO user) {
+        DAOUser newUser = new DAOUser();
+        newUser.setUsername(user.getUsername());
+        newUser.setPassword(bcryptEncoder.encode(user.getPassword()));
+        return userDao.save(newUser);
+    }
+}
